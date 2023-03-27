21c72f3885b1411fd3ab7f7fd32ddf43061e599f
MariaEduardaSM <mariadudamiguel28@gmail.com>
2023-03-27 20:17:24 -0300

    Aula 27/03/23

---
+++ App.tsx
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+
+import React,{Component} from "react";
+import { View, Text,StyleSheet } from "react-native";
+import Simples from"<div className="" />
+<componentes />
+<simples></simples>
+
+export default class app extends Component{
+render(){
+  return(
+      <View style={estilos.container}>
+   
+
+      <Text style={estilos.fonte}>Maria Eduarda</Text>
+      </View>
+  );
+
+  }
+}
+
+const estilos = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent:'center',
+    alignItems: 'center'
+    
+
+  },
+  fonte:{
+    fontSize:50,
+    
+  }
+  
+  });
